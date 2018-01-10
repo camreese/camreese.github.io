d2af30c989edc386adc18aee23e232f2265d08e3
woodshedpedals <cameronreese@Camerons-MacBook-Pro.local>
2018-01-10 06:16:06 -0500

    Update audio player
    Add new song

--- js/playlists.js
+++ js/playlists.js
@@ -4,4 +4,10 @@ var playlist_01 = [{
     "title":"Awakening",
     "audio":"/audio/Awakening.mp3",
     "cover":"/images/native-shadows.jpeg"
+}, {
+	"type":"audio",
+    "artist":"Warpath",
+    "title":"The Trooper (Cover)",
+    "audio":"/audio/Trooper.mp3",
+    "cover":"/images/Warpath.jpg"
 }];
