af41f4670627a100a4f0f128263e4ab6a27af22b
woodshedpedals <cameronreese@Camerons-MacBook-Pro.local>
2018-01-14 18:38:44 -0500

    change order
    change order

--- js/playlists.js
+++ js/playlists.js
@@ -1,13 +1,13 @@
 var playlist_01 = [{
     "type":"audio",
-    "artist":"Native Shadows",
-    "title":"Awakening",
-    "audio":"/audio/Awakening.mp3",
-    "cover":"/images/native-shadows.jpeg"
-}, {
-	"type":"audio",
     "artist":"Warpath",
     "title":"The Trooper (Cover)",
     "audio":"/audio/Trooper.mp3",
     "cover":"/images/Warpath.jpg"
+}, {
+    "type":"audio",
+    "artist":"Native Shadows",
+    "title":"Awakening",
+    "audio":"/audio/Awakening.mp3",
+    "cover":"/images/native-shadows.jpeg"
 }];
